93eed1e7e726e03eb281789d3e4137a6f21c4081
marcilia.guilger@ifood.com.br <marcilia.guilger@ifood.com.br>
2024-09-28 19:04:48 -0300

    v0

--- dog-cluster-2/dog-eks-cluster/main.tf
+++ dog-cluster-2/dog-eks-cluster/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-east-1"
   profile = "pos"
 }
+
 # Criar a VPC
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
@@ -109,33 +110,6 @@ resource "aws_route_table_association" "private_b" {
   route_table_id = aws_route_table.private.id
 }
 
-
-# Configurar o API Gateway (Exemplo Simples)
-resource "aws_api_gateway_rest_api" "example" {
-  name        = "example-api"
-  description = "Example API Gateway"
-}
-
-resource "aws_api_gateway_resource" "example_resource" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-  parent_id   = aws_api_gateway_rest_api.example.root_resource_id
-  path_part   = "example"
-}
-
-resource "aws_api_gateway_method" "example_method" {
-  rest_api_id   = aws_api_gateway_rest_api.example.id
-  resource_id   = aws_api_gateway_resource.example_resource.id
-  http_method   = "GET"
-  authorization = "NONE"
-}
-
-resource "aws_api_gateway_integration" "example_integration" {
-  rest_api_id = aws_api_gateway_rest_api.example.id
-  resource_id = aws_api_gateway_resource.example_resource.id
-  http_method = aws_api_gateway_method.example_method.http_method
-  type        = "MOCK"
-}
-
 resource "aws_api_gateway_deployment" "example_deployment" {
   depends_on = [
     aws_api_gateway_integration.example_integration
@@ -156,6 +130,7 @@ resource "aws_nat_gateway" "nat" {
 Name = "main-nat"
   }
 }
+
 # Adicionar rota para o NAT Gateway na Tabela de Rotas Privada
 resource "aws_route" "private_nat_gateway" {
   route_table_id         = aws_route_table.private.id
